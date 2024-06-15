937b36901b89cf94540f0d5d176da6386b60f131
Majid Ash <Magid.Ash.Si@gmail.com>
2024-06-15 10:54:00 -0700

    Create several methods for making cards

--- models/products.js
+++ models/products.js
@@ -11,29 +11,50 @@ class Products {
   createCard(data) {
     const cardEle = document.createElement("div");
 
-    const img = document.createElement("img");
+    const imgEle = this.productImg(data);
+    const infoEle = this.productInfo(data);
 
-    img.src = data.image;
-    img.alt = data.alt;
+    cardEle.innerHTML = imgEle;
 
-    cardEle.appendChild(img);
-
-    const info = document.createElement("div");
-    const productName = document.createElement("h3");
-    const control = document.createElement("div");
-    const price = document.createElement("span");
-    const button = document.createElement("span");
-
-    productName.innerText = data.name;
-    price.innerText = `$ ${data.price}`;
-    button.innerText = "+";
-
-    control.append(price, button);
-    info.append(productName, control);
-
-    cardEle.appendChild(info);
+    cardEle.innerHTML += infoEle;
 
     this.parent.appendChild(cardEle);
+  }
+
+  productImg(data) {
+    const { image, alt } = data;
+
+    const imgJSX = `<img src=${image} alt=${alt}/>`;
+
+    return imgJSX;
+  }
+
+  productInfo(data) {
+    // const info = document.createElement("div");
+    // const productName = document.createElement("h3");
+    // const control = document.createElement("div");
+    // const price = document.createElement("span");
+    // const button = document.createElement("span");
+
+    // productName.innerText = data.name;
+    // price.innerText = `$ ${data.price}`;
+    // button.innerText = "+";
+
+    // control.append(price, button);
+    // info.append(productName, control);
+
+    const { id, name, price } = data;
+
+    const infoJSX = `
+    <div>
+        <h3>${name}</h3>
+        <div>
+            <span>$ ${price}</span>
+            <button data-id=${id}>+</button>
+        </div>
+    </div>`;
+
+    return infoJSX;
   }
 }
 
